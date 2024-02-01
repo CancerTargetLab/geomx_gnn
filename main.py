12165438188443bb0a403aecb99282787ad07c44
mjheid <m.j.heidrich19@gmail.com>
2024-02-01 16:13:49 +0100

    Start working on adding desc to args in main

--- main.py
+++ main.py
@@ -4,59 +4,105 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Arguments for image and GNN models")
 
     # Arguments for Image preprocessing
-    parser.add_argument("--preprocess_dir", type=str, default="data/raw/p2106")
-    parser.add_argument("--image_preprocess", action="store_true", default=False)
+    parser.add_argument("--preprocess_dir", type=str, default="data/raw/p2106",
+                        help="Directory in which .tiff files are for preprocessing")
+    parser.add_argument("--image_preprocess", action="store_true", default=False,
+                        help="Wether or not to preprocess images via ZScore normalisation")
 
     # Arguments for image model
-    parser.add_argument("--image_dir", type=str, default="data/raw/TMA1_preprocessed")
-    parser.add_argument("--batch_size_image", type=int, default=256)
-    parser.add_argument("--epochs_image", type=int, default=100)
-    parser.add_argument("--warmup_epochs_image", type=int, default=10)
-    parser.add_argument("--num_workers_image", type=int, default=1)
-    parser.add_argument("--lr_image", type=float, default=0.1)
-    parser.add_argument("--embedding_size_image", type=int, default=256)
-    parser.add_argument("--contrast_size_image", type=int, default=124)
-    parser.add_argument("--early_stopping_image", type=int, default=100)
-    parser.add_argument("--crop_factor", type=float, default=0.5)
-    parser.add_argument("--train_ratio_image", type=float, default=0.6)
-    parser.add_argument("--val_ratio_image", type=float, default=0.2)
-    parser.add_argument("--resnet_model", type=str, default="18")
-    parser.add_argument("--output_name_image", type=str, default="out/image_contrast.pt")
-    parser.add_argument("--train_image_model", action="store_true", default=False)
-    parser.add_argument("--embed_image_data", action="store_true", default=False)
+    parser.add_argument("--image_dir", type=str, default="data/raw/TMA1_preprocessed",
+                        help="Directory in which preproccessed Images lay")
+    parser.add_argument("--batch_size_image", type=int, default=256,
+                        help="Number of Cell Images per Batch")
+    parser.add_argument("--epochs_image", type=int, default=100,
+                        help="Number of epochs for which to train")
+    parser.add_argument("--warmup_epochs_image", type=int, default=10,
+                        help="Number of Epochs in which learning rate gets increased")
+    parser.add_argument("--num_workers_image", type=int, default=1,
+                        help="Number of worker processes to be used(loading data etc)")
+    parser.add_argument("--lr_image", type=float, default=0.1,
+                        help="Learning rate of model")
+    parser.add_argument("--embedding_size_image", type=int, default=256,
+                        help="Linear net size used to embed data")
+    parser.add_argument("--contrast_size_image", type=int, default=124,
+                        help="Linear net size on which to calculate the contrast loss")
+    parser.add_argument("--early_stopping_image", type=int, default=100,
+                        help="Number of epochs after which to stop model run without improvement to val contrast loss")
+    parser.add_argument("--crop_factor", type=float, default=0.5,
+                        help="Cell Image crop factor for Image augmentation")
+    parser.add_argument("--train_ratio_image", type=float, default=0.6,
+                        help="Ratio of Cell Images upon which to train")
+    parser.add_argument("--val_ratio_image", type=float, default=0.2,
+                        help="Ratio of Cell Images upon which to Validate")
+    parser.add_argument("--resnet_model", type=str, default="18",
+                        help="What ResNet model to choose, on of 18, 34, 50 and 101")
+    parser.add_argument("--output_name_image", type=str, default="out/image_contrast.pt",
+                        help="Name of model")
+    parser.add_argument("--train_image_model", action="store_true", default=False,
+                        help="Wether or not to train the Image model")
+    parser.add_argument("--embed_image_data", action="store_true", default=False,
+                        help="Wether or not to embed data with a given Image model")
 
     # Arguments for the GNN model
-    parser.add_argument("--graph_dir", type=str, default="data/")
-    parser.add_argument("--graph_raw_subset_dir", type=str, default="TMA1_preprocessed")
-    parser.add_argument("--graph_label_data", type=str, default="OC1_all.csv")
-    parser.add_argument("--data_is_log_graph", action="store_true", default=False)
-    parser.add_argument("--batch_size_graph", type=int, default=1)
-    parser.add_argument("--epochs_graph", type=int, default=100)
+    parser.add_argument("--graph_dir", type=str, default="data/",
+                        help="Where to find the raw/ and processed/ dirs")
+    parser.add_argument("--graph_raw_subset_dir", type=str, default="TMA1_preprocessed",
+                        help="How the subdir in raw/ and processed/ is called")
+    parser.add_argument("--graph_label_data", type=str, default="OC1_all.csv",
+                        help=".csv label data in the raw dir containing count data")
+    parser.add_argument("--data_is_log_graph", action="store_true", default=False,
+                        help="Wether or not the count data is log or not")
+    parser.add_argument("--batch_size_graph", type=int, default=1,
+                        help="Number of Graphs per Batch")
+    parser.add_argument("--epochs_graph", type=int, default=100,
+                        help="Number of epochs for which to train")
     parser.add_argument("--warmup_epochs_graph", type=int, default=10)
-    parser.add_argument("--num_workers_graph", type=int, default=1)
-    parser.add_argument("--lr_graph", type=float, default=0.005)
-    parser.add_argument("--early_stopping_graph", type=int, default=10)
-    parser.add_argument("--train_ratio_graph", type=float, default=0.6)
-    parser.add_argument("--val_ratio_graph", type=float, default=0.2)
-    parser.add_argument("--node_dropout", type=float, default=0.3)
-    parser.add_argument("--edge_dropout", type=float, default=0.5)
-    parser.add_argument("--graph_model_type", type=str, default="GAT")
-    parser.add_argument("--graph_mse_mult", type=float, default=1.0)
-    parser.add_argument("--graph_cos_sim_mult", type=float, default=1.0)
-    parser.add_argument("--graph_entropy_mult", type=float, default=1.0)
+    parser.add_argument("--num_workers_graph", type=int, default=1,
+                        help="Number of worker processes to be used(loading data etc)")
+    parser.add_argument("--lr_graph", type=float, default=0.005,
+                        help="Learning rate of model")
+    parser.add_argument("--early_stopping_graph", type=int, default=10,
+                        help="Number of epochs without validation loss improvement after which to stop training")
+    parser.add_argument("--train_ratio_graph", type=float, default=0.6,
+                        help="Ratio of Patients used for training")
+    parser.add_argument("--val_ratio_graph", type=float, default=0.2,
+                        help="Ratio of Patients which are used for validation")
+    parser.add_argument("--node_dropout", type=float, default=0.3,
+                        help="Probability of Graph Node dropout during training")
+    parser.add_argument("--edge_dropout", type=float, default=0.5,
+                        help="Probability of Graph Edge dropout during training")
+    parser.add_argument("--graph_model_type", type=str, default="GAT",
+                        help="Type of Model to train, one of GAT, GAT_ph, LIN, LIN_ph")
+    parser.add_argument("--graph_mse_mult", type=float, default=1.0,
+                        help="Multiplier for MSE Loss")
+    parser.add_argument("--graph_cos_sim_mult", type=float, default=1.0,
+                        help="Multiplier for Cosine Similarity Loss")
+    parser.add_argument("--graph_entropy_mult", type=float, default=1.0,
+                        help="Multiplier for Entropy Loss")
     #parser.add_argument("--num_phenotypes_graph", type=int, default=15)
     #parser.add_argument("--num_phenotypes_layers_graph", type=int, default=2)
-    parser.add_argument("--layers_graph", type=int, default=1)
-    parser.add_argument("--num_node_features", type=int, default=256)
-    parser.add_argument("--num_edge_features", type=int, default=1)
-    parser.add_argument("--num_embed_features", type=int, default=128)
-    parser.add_argument("--heads_graph", type=int, default=1)
-    parser.add_argument("--embed_dropout_graph", type=float, default=0.1)
-    parser.add_argument("--conv_dropout_graph", type=float, default=0.1)
-    parser.add_argument("--output_name_graph", type=str, default="out/ROI.pt")
-    parser.add_argument("--train_gnn", action="store_true", default=False)
-    parser.add_argument("--embed_gnn_data", action="store_true", default=False)
-    parser.add_argument("--output_graph_embed", type=str, default="out/")
+    parser.add_argument("--layers_graph", type=int, default=1,
+                        help="Number of Layers in Graph")
+    parser.add_argument("--num_node_features", type=int, default=256,
+                        help="Size of initial Node features")
+    parser.add_argument("--num_edge_features", type=int, default=1,
+                        help="Size of edge features")
+    parser.add_argument("--num_embed_features", type=int, default=128,
+                        help="Size to embed initial Node features to")
+    parser.add_argument("--heads_graph", type=int, default=1,
+                        help="Number of Attention Heads for the Graph NN")
+    parser.add_argument("--embed_dropout_graph", type=float, default=0.1,
+                        help="Percentage of embedded feature dropout chance")
+    parser.add_argument("--conv_dropout_graph", type=float, default=0.1,
+                        help="Percentage of dropout chance between layers")
+    parser.add_argument("--output_name_graph", type=str, default="out/ROI.pt",
+                        help="Name of model")
+    parser.add_argument("--train_gnn", action="store_true", default=False,
+                        help="Wther or not to train the Graph Model")
+    parser.add_argument("--embed_gnn_data", action="store_true", default=False,
+                        help="Wether or not to embed predicted Cell Expression")
+    parser.add_argument("--output_graph_embed", type=str, default="out/",
+                        help="Dir in which to embed Cell Expressions")
 
     # Arguments for the TME model
     parser.add_argument("--tme_dir", type=str, default="data/")
@@ -88,8 +134,10 @@ def parse_args():
     parser.add_argument("--embed_tme_data", action="store_true", default=False)
     parser.add_argument("--output_tme_embed", type=str, default="out/")
 
-    parser.add_argument("--deterministic", action="store_true", default=True)
-    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--deterministic", action="store_true", default=True,
+                        help="Wether or not to run NNs deterministicly")
+    parser.add_argument("--seed", type=int, default=42,
+                        help="Seed for random computations")
 
     # Visualize Expression 
     parser.add_argument("--visualize_expression", action="store_true", default=False)
