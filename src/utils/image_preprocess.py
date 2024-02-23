fix bug for image mean and std calculation

src/utils/image_preprocess.py
@@ -29,12 +29,12 @@ def calc_mean_std(image_paths, max_img=2**16, img_channels=''):
         img = load_img(img_p, img_channels=img_channels)
         local_hist = [np.histogram(img[:,:,channel], bins=max_img+2, range=(0,max_img+2)) for channel in range(img.shape[2])]
         if global_hist:
-            global_hist = [np.concatenate((global_hist[channel], local_hist[channel])) for channel in range(len(local_hist))]
+            global_hist = [global_hist[channel][0]+local_hist[channel][0] for channel in range(len(local_hist))]
         else:
             global_hist = local_hist
     
-    mean = np.array([np.mean(hist_chan[0]*hist_chan[1][:hist_chan[1].shape[0]-1]) for hist_chan in global_hist], dtype=np.float32)
-    std = np.array([np.std(hist_chan[0]*hist_chan[1][:hist_chan[1].shape[0]-1]) for hist_chan in global_hist], dtype=np.float32)
+    mean = np.array([np.sum(hist_chan[0]*hist_chan[1][:hist_chan[1].shape[0]-1])/np.sum(hist_chan[0]) for hist_chan in global_hist], dtype=np.float32)
+    std = np.array([np.sqrt(np.sum((global_hist[chan][0]-mean[chan])**2)/np.sum(global_hist[chan][0])) for chan in range(len(global_hist))], dtype=np.float32)
     return mean, std
 
 def zscore(image_paths, mean, std):