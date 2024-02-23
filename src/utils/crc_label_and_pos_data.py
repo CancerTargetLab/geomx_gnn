10c84598f492a918b24d7ffe4c2302c9c1cf1312
mjheid <m.j.heidrich19@gmail.com>
2024-02-23 13:48:01 +0100

    CRC preproccesing bug fixes

--- src/utils/crc_label_and_pos_data.py
+++ src/utils/crc_label_and_pos_data.py
@@ -9,10 +9,10 @@ pos_df = pd.DataFrame()
 label_df = pd.DataFrame()
 
 for csv in csvs:
-    df = pd.read_csv(csv, index=0, sep=',')
-    df['Image'] = np.array([csv.split('.')[0]+'.ome.tif']*df.shape[0])
+    df = pd.read_csv(csv)
+    df['Image'] = np.array([csv.split('/')[-1].split('.')[0]+'.ome.tif']*df.shape[0])
 
-    gene_names = np.concatenate((np.array(['Hoechst1']),df.columns[9:-9].values))
+    gene_names = np.concatenate((np.array(['Hoechst1']),df.columns[9:-10].values))
     genes_sum = np.sum(df[gene_names].values, axis=0)
     if pos_df.shape[0]>0:
         tmp_df = pd.DataFrame()
@@ -23,18 +23,36 @@ for csv in csvs:
         pos_df = pd.concat([pos_df, tmp_df], ignore_index=True)
 
         tmp_df = pd.DataFrame()
-        tmp_df['ROI'] = [csv.split('.')[0]]
+        tmp_df['ROI'] = [csv.split('.')[0].split('/')[-1]]
         tmp_df['Patiend_ID'] = [csv.split('.')[0][-2:]]
         tmp_df[gene_names] = [genes_sum]
+        label_df = pd.concat([label_df, tmp_df], ignore_index=True)
     else:
         pos_df['Image'] = df['Image']
         pos_df['Centroid.X.px'] = df['Xt']
         pos_df['Centroid.Y.px'] = df['Yt']
         pos_df['Class'] = ''
 
-        label_df['ROI'] = [csv.split('.')[0]]
+        label_df['ROI'] = [csv.split('.')[0].split('/')[-1]]
         label_df['Patiend_ID'] = [csv.split('.')[0][-2:]]
         label_df[gene_names] = [genes_sum]
 
 pos_df.to_csv('data/raw/CRC/CRC_measurements.csv', sep=',', header=True, index=False,)
 label_df.to_csv('data/raw/CRC/CRC_label.csv', sep=',', header=True, index=False,)
+
+def plot_gene_histo(gene):
+    import matplotlib.pyplot as plt
+
+    csv_dir = os.path.join(os.getcwd(), 'data/raw/CRC/')
+    csvs = [os.path.join(csv_dir,p) for p in os.listdir(csv_dir) if p.endswith('.csv')]
+    for csv in csvs:
+        print(f'Plotting {csv} ...')
+        df = pd.read_csv(csv)
+        plt.hist(df[gene].values, bins='auto', color='blue',)
+        plt.savefig(csv.split('/')[-1].split('.')[0]+'.png')
+        plt.close()
+        area = df['AREA'].values
+        print('Area Max: ',np.max(area))
+        print('Area Mean: ',np.mean(area))
+        print('Area Median: ', np.median(area))
+    print('DONE')
