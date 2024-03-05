2aff88175469498b667fa9a336f6898453a879f9
mjheid <m.j.heidrich19@gmail.com>
2024-03-05 16:09:32 +0100

    calculate corr between cell preds and truth

---
+++ src/explain/correlation.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+import scanpy as sc
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.utils.stats import per_gene_corr
+
+adata_name = '_crc_lin_sub'
+og_df_name = 'CRC_measurements.csv'
+out = 'figrues/crc_lin_sub'
+
+df = pd.read_csv(os.path.join('data/raw/CRC', adata_name, header=0, sep=','))
+
+adata = sc.read_h5ad(os.path.join('out', adata_name+'_all.h5ad'))
+var_names = adata.var_names.values
+
+pred = adata.X
+y = df[df.columns[4:].values].values
+
+p_statistic, p_pval = per_gene_corr(pred, y, mean=False, method='pearsonr')
+s_statistic, s_pval = per_gene_corr(pred, y, mean=False, method='pearsonr')
+k_statistic, k_pval = per_gene_corr(pred, y, mean=False, method='pearsonr')
+    
+correlation_data = {
+    'Variable': var_names,
+    'Pearson Correlation Coef.': [corr for corr in p_statistic],
+    'Pearson p-value': [corr for corr in p_pval],
+    'Spearman Correlation Coef.': [corr for corr in s_statistic],
+    'Spearman p-value': [corr for corr in s_pval],
+    'Kendall Correlation Coef.': [corr for corr in k_statistic],
+    'Kendall p-value': [corr for corr in k_pval]
+}
+
+corr_df = pd.DataFrame(correlation_data)
+
+plt.figure(figsize=(10, 5))
+plt.table(cellText=df.values, colLabels=df.columns, loc='center')
+plt.axis('off')
+plt.savefig(os.path.join(out, 'corr'+adata_name+'.png'))
--- src/utils/stats.py
+++ src/utils/stats.py
@@ -1,12 +1,24 @@
 import numpy as np
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, spearmanr, kendalltau
 
 def per_gene_pcc(x, y, mean=True):
+    #print(f'Use src.utils.stats.per_gene_corr instead of this')
+    return per_gene_corr(x, y, mean=mean, method='pearsonr')
+    
+def per_gene_corr(x, y, mean=True, method='pearsonr'):
+    if method.upper() == 'PEARSONR':
+        corr = pearsonr
+    elif method.upper() == 'SPEARMANR':
+        corr = spearmanr
+    elif  method.upper() == 'KENDALLTAU':
+        corr = kendalltau
+    else:
+        raise Exception(f'Method {method} not one of pearsonr, spearmanr or kendalltau')
     x, y = x.astype(np.float64), y.astype(np.float64)
     statistic = np.ndarray(x.shape[-1])
     pval = np.ndarray(x.shape[-1])
     for gene in range(x.shape[-1]):
-        pcc = pearsonr(x[:,gene], y[:,gene])
+        pcc = corr(x[:,gene], y[:,gene])
         statistic[gene], pval[gene] = pcc.statistic, pcc.pvalue
     if mean:
         return np.nanmean(statistic), np.nanmean(pval)
