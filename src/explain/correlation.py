4c210f1ef7affa6abfd4bd24cca064f1231b27cd
mjheid <m.j.heidrich19@gmail.com>
2024-03-06 11:54:52 +0100

    sort by files in corr

--- src/explain/correlation.py
+++ src/explain/correlation.py
@@ -10,11 +10,19 @@ og_df_name = 'CRC_measurements.csv'
 out = 'figrues/crc_lin_sub'
 
 df = pd.read_csv(os.path.join('data/raw/CRC', adata_name, header=0, sep=','))
+df['Image'] = df['Image'].apply(lambda x: x.split('.')[0])
+df = df.sort_values(by='Image').reset_index(drop=True)
 
 adata = sc.read_h5ad(os.path.join('out', adata_name+'_all.h5ad'))
 var_names = adata.var_names.values
+tmp = pd.DataFrame()
+tmp['files'] = adata.obs['files'].values
+tmp[var_names] = adata.X
+adata = tmp
+adata['files'] = adata['files'].apply(lambda x: x.split('_')[-1].split('.')[0])
+adata = adata.sort_values(by='files').reset_index(drop=True)
 
-pred = adata.X
+pred = adata[adata.columns[1:].values].values
 y = df[df.columns[4:].values].values
 
 p_statistic, p_pval = per_gene_corr(pred, y, mean=False, method='pearsonr')
@@ -36,4 +44,4 @@ corr_df = pd.DataFrame(correlation_data)
 plt.figure(figsize=(10, 5))
 plt.table(cellText=corr_df.values, colLabels=corr_df.columns, loc='center')
 plt.axis('off')
-plt.savefig(os.path.join(out, 'corr'+adata_name+'.png'))
+plt.savefig(os.path.join(out, 'corr'+adata_name+'.pdf'))
