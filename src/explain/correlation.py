b2046180c2a593f63fd313d705f4e884b61fc6d4
mjheid <markus.heidrich@immun.lth.se>
2024-10-11 16:51:02 +0200

    fix bug

--- src/explain/correlation.py
+++ src/explain/correlation.py
@@ -11,7 +11,10 @@ def correlation(raw_subset_dir='CRC',
     adata_name = vis_name
     out = figure_dir
 
-    df = pd.read_csv([p for p in os.listdir(os.path.join('data/raw/', raw_subset_dir)) if p.endswith('.csv')][0],
+    df = pd.read_csv([os.path.join('data/raw/',
+                                   raw_subset_dir,
+                                   p) for p in os.listdir(os.path.join('data/raw/',
+                                                                       raw_subset_dir)) if p.endswith('.csv')][0],
                      header=0,
                      sep=',')
     df['Image'] = df['Image'].apply(lambda x: x.split('.')[0])
