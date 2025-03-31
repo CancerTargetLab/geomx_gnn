888919b0418e99ed30510e3d2975b91de4b8b5c8
mjheid <markus.heidrich@immun.lth.se>
2025-03-31 09:15:28 +0200

    small stuff

--- src/utils/impute_w_dca.py
+++ src/utils/impute_w_dca.py
@@ -9,7 +9,7 @@ df = pd.read_csv(measurements)
 adata = sc.AnnData(df.loc[np.sum(df[df.columns[4:]].values, axis=-1) > 0, df.columns[4:]].values)
 adata_var_names = df.columns[4:].values
 
-sc.external.pp.dca(adata, ae_type='zinb-conddisp', verbose=True, return_info=True, learning_rate=0.001, batch_size=256)
+sc.external.pp.dca(adata, ae_type='zinb-conddisp', verbose=True, return_info=True, learning_rate=0.0005, batch_size=256, hidden_size=(256, 128, 64, 128, 256))
 
-df.loc[np.sum(df[df.columns[4:]].values, axis=-1) > 0, df.columns[4:].values] = adata.X
+df.loc[np.sum(df[df.columns[4:]].values, axis=-1) > 0, df.columns[4:].values] = adata.X.astype(np.float32)
 df.to_csv(out, index=False, header=True, sep=',')
